e3a4121487e6eb2c66ae740daf3338cc53a0e7c0
Marv Niederhaus <mniederhas@aol.com>
2014-06-16 19:44:22 -0400

    Added to static pages title and background

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+# Use Capybara function 'visit' and Capybara variable 'page' for 
+# testing static pages.
+describe "StaticPages" do
+
+	describe "Home page" do
+		it "should have the content 'Sample App'" do
+			visit '/static_pages/home'
+			expect(page).to have_content('Sample App')
+		end
+		it "should have the title Home" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Sample App | Home")
+		end
+	end
+
+	describe "Help page" do
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_content('Help')
+		end
+		it "should have the title Help" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Sample App | Help")
+		end
+	end
+
+	describe "About Us page" do
+		it "should have the content 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_content('About Us')
+		end
+		it "should have the title About Us" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Sample App | About Us")
+		end
+	end
+
+end
